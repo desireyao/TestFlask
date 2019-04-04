4d60367cf3cf35784c98daef86c8d56c49249505
yaoh <yaoh.yao@beacool.com>
2019-04-04 13:53:57 +0800

    区分 config 配置

--- config/config.py
+++ config/config.py
@@ -4,12 +4,16 @@ class Config(object):
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 
-class TestingConfig(Config):
+class ProductConfig(Config):
     SQLALCHEMY_DATABASE_URI = 'mysql://root:asdf1234@127.0.0.1:3306/tnews'
 
 
+class TestingConfig(Config):
+    SQLALCHEMY_DATABASE_URI = 'mysql://root:asdf1234@127.0.0.1:3306/tnews'
+
 Config = {
-    'testing': TestingConfig
+    'testing': TestingConfig,
+    'procuction': ProductConfig
 }
 
 # 只需改变此处 配置当前的环境
